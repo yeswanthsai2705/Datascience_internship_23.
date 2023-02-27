1dfa9c63b8fda989872c7eb92b70a39f9b05366e
Yeswanth sai yadla <94543732+yeswanthsai2705@users.noreply.github.com>
2023-02-27 16:47:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,23 @@
 import streamlit as st
 
-st.header('Hello world')
-st.header('This is part of task of data science internship')
+st.title(":red[INNOMATICS] Data App")
+
+st.header('This is a task for the internship')
+st.subheader('Task is done in FEB 2023')
+
+
+
+
+CODE = ''' print("hello world") '''
+st.code(CODE, language='python')
+
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(
+    np.random.randn(50,20),
+    columns=('col %d' % i for i in range(20))
+    )
+
+
+st.dataframe(df)
